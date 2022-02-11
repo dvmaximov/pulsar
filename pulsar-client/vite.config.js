8ea63db0e4d2001d930588281744493b37e99fb7
dvmaximov <dvmaximov@mail.ru>
2022-02-11 17:17:22 +0300

    Updated

--- pulsar-client/vite.config.js
+++ pulsar-client/vite.config.js
@@ -1,15 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// const mode = process.env === "production";
+
+console.log("mode", import.meta.env);
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  envDir: "./",
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        dir: "../docs",
+// export default defineConfig({
+//   envDir: "./",
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         dir: "../docs",
+//       },
+//     },
+//   },
+// });
+
+export default defineConfig(({ command, mode }) => {
+  if (command === "serve") {
+    return {
+      envDir: "./",
+      plugins: [react()],
+      build: {
+        rollupOptions: {
+          output: {
+            dir: "../docs",
+          },
+        },
       },
-    },
-  },
+    };
+  } else {
+    // command === 'build'
+    return {
+      envDir: "./",
+      base: "/pulsar/",
+      plugins: [react()],
+      build: {
+        rollupOptions: {
+          output: {
+            dir: "../docs",
+          },
+        },
+      },
+    };
+  }
 });
